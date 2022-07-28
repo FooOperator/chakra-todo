e0352b39886b91bee0a21e4cad25f4dac67b5f22
FooOperator <foxoperator1@gmail.com>
2022-07-28 09:40:53 +0100

    Added toast on rename

--- src/components/EditTitle.tsx
+++ src/components/EditTitle.tsx
@@ -1,88 +1,95 @@
 import { EditIcon } from "@chakra-ui/icons";
 import {
-  Button,
-  Flex,
-  IconButton,
-  Input,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  useColorModeValue,
-  useDisclosure,
+	Button,
+	Flex,
+	IconButton,
+	Input,
+	Modal,
+	ModalBody,
+	ModalCloseButton,
+	ModalContent,
+	ModalFooter,
+	ModalHeader,
+	ModalOverlay,
+	useColorModeValue,
+	useDisclosure,
+	useToast,
 } from "@chakra-ui/react";
 import { useRef, useState } from "react";
 import useStore from "../lib/store";
 import { ToDo } from "../types/store";
 
 interface EditTitleProps {
-  todo: ToDo;
+	todo: ToDo;
 }
 
 const EditTitle: React.FC<EditTitleProps> = ({ todo }) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const { update } = useStore();
-  const [newToDo, setNewToDo] = useState<string>("");
-  const modalBg = useColorModeValue("gray.400", "gray.900");
-  const initialFocusRef = useRef(null);
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const { update } = useStore();
+	const toast = useToast();
+	const [newToDo, setNewToDo] = useState<string>("");
+	const modalBg = useColorModeValue("gray.400", "gray.900");
+	const initialFocusRef = useRef(null);
 
-  const handleEdit = () => {
-    update({
-      ...todo,
-      title: newToDo,
-    });
-    setNewToDo("");
-    onClose();
-  };
+	const handleEdit = () => {
+		update({
+			...todo,
+			title: newToDo,
+		});
+		toast({
+			title: `Renamed '${todo.title}' to '${newToDo}'`,
+			duration: 1500,
+		});
+		setNewToDo("");
+		onClose();
+	};
 
-  const handleChange = (e: any) => setNewToDo(e.currentTarget.value);
+	const handleChange = (e: any) => setNewToDo(e.currentTarget.value);
 
-  return (
-    <>
-      <IconButton
-        aria-label="edit todo"
-        icon={<EditIcon />}
-        onClick={onOpen}
-        colorScheme="linkedin"
-      />
-      <Modal
-        initialFocusRef={initialFocusRef}
-        isOpen={isOpen}
-        onClose={onClose}
-      >
-        <ModalOverlay />
-        <ModalContent backgroundColor={modalBg}>
-          <ModalHeader>Editing Title</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Flex direction={"column"}>
-              <Input
-                ref={initialFocusRef}
-                value={newToDo}
-                onChange={handleChange}
-                placeholder={todo.title}
-              />
-            </Flex>
-          </ModalBody>
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Cancel
-            </Button>
-            <Button
-              colorScheme={"green"}
-              disabled={newToDo.length < 1}
-              onClick={handleEdit}
-            >
-              Edit
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </>
-  );
+	return (
+		<>
+			<IconButton
+				aria-label="edit todo"
+				icon={<EditIcon />}
+				onClick={onOpen}
+				colorScheme="linkedin"
+			/>
+			<Modal
+				initialFocusRef={initialFocusRef}
+				isOpen={isOpen}
+				onClose={onClose}>
+				<ModalOverlay />
+				<ModalContent backgroundColor={modalBg}>
+					<ModalHeader>Editing Title</ModalHeader>
+					<ModalCloseButton />
+					<ModalBody>
+						<Flex direction={"column"}>
+							<Input
+								ref={initialFocusRef}
+								value={newToDo}
+								onChange={handleChange}
+								placeholder={todo.title}
+							/>
+						</Flex>
+					</ModalBody>
+					<ModalFooter>
+						<Button
+							colorScheme="blue"
+							mr={3}
+							onClick={onClose}>
+							Cancel
+						</Button>
+						<Button
+							colorScheme={"green"}
+							disabled={newToDo.length < 1}
+							onClick={handleEdit}>
+							Edit
+						</Button>
+					</ModalFooter>
+				</ModalContent>
+			</Modal>
+		</>
+	);
 };
 
 export default EditTitle;
