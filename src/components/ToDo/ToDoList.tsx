1399a02b3142f76d78aa3b5d832c28352c006d56
FooOperator <foxoperator1@gmail.com>
2022-07-28 09:40:31 +0100

    Cleared unused imports
    Added subscription that activates toasts:
    
    - Add
    - Remove
    - Done checked

--- src/components/ToDo/ToDoList.tsx
+++ src/components/ToDo/ToDoList.tsx
@@ -1,37 +1,91 @@
 import {
-	List,
-	Divider,
-	Tabs,
-	TabList,
-	Tab,
-	Stack,
-	TabPanels,
-	TabPanel,
-	ListItem,
-	Text,
-	Flex,
-	Checkbox,
 	Button,
 	Container,
+	Divider,
+	Fade,
+	Flex,
+	List,
+	ListItem,
+	Stack,
+	Tab,
+	TabList,
+	TabPanel,
+	TabPanels,
+	Tabs,
+	Text,
+	useToast,
+	UseToastOptions,
 } from "@chakra-ui/react";
-import { ChangeEvent, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import useStore from "../../lib/store";
 import { ToDo } from "../../types/store";
 import ToDoItem from "./ToDoItem";
 
 const ToDoList: React.FC = () => {
+	const toast = useToast();
 	const [tabIndex, setTabIndex] = useState<number>(0);
 	const todos = useStore((state) => state.todos);
-
 	const tabPanelsContent = [
 		todos,
 		todos.filter((todo) => !todo.done),
 		todos.filter((todo) => todo.done),
 	];
 
-	const [currentTabContent, setCurrentTabContent] = useState<
-		ToDo[]
-	>(() => []);
+	const [currentTabContent, setCurrentTabContent] = useState<ToDo[]>(
+		() => []
+	);
+
+	useEffect(() => {
+		const todosSubscription = useStore.subscribe((state, prev) => {
+			const currTodos = state.todos;
+			const prevTodos = prev.todos;
+
+			if (currTodos.length > prevTodos.length) {
+				showToast({
+					title: `'${currTodos[0].title}' added`,
+				});
+				return;
+			}
+
+			if (currTodos.length < prevTodos.length) {
+				const removed = prevTodos.filter(
+					(x) => !currTodos.includes(x)
+				);
+				const amountRemoved = removed.length;
+				showToast({
+					title:
+						amountRemoved > 1
+							? `${amountRemoved} todos removed`
+							: `'${removed[0].title}' removed`,
+				});
+
+				return;
+			}
+
+			const doneCurr = currTodos.filter((todo) => todo.done);
+			const donePrev = prevTodos.filter((todo) => todo.done);
+
+			if (doneCurr.length > donePrev.length) {
+				showToast({
+					title: `'${doneCurr.slice(-1)[0].title}' markedz done`,
+				});
+				return;
+			}
+		});
+
+		return () => {
+			todosSubscription();
+		};
+	}, []);
+
+	const showToast = (options: UseToastOptions) => {
+		toast({
+			...options,
+			status: options.status ?? "success",
+			colorScheme: "pink",
+			duration: options.duration ?? 1500,
+		});
+	};
 
 	const handleTabsChange = (index: number) => {
 		setTabIndex(index);
@@ -46,9 +100,7 @@ const ToDoList: React.FC = () => {
 				variant={"solid-rounded"}
 				defaultIndex={1}
 				colorScheme={"red"}>
-				<TabList
-					display="flex"
-					justifyContent={"center"}>
+				<TabList display="flex" justifyContent={"center"}>
 					<Tab>All</Tab>
 					<Tab>To Do</Tab>
 					<Tab>Done</Tab>
@@ -63,11 +115,10 @@ const ToDoList: React.FC = () => {
 										borderRadius: "8px",
 										backgroundColor: `gray.100`,
 									},
-									"&::-webkit-scrollbar-thumb":
-										{
-											backgroundColor: `red.600`,
-											borderRadius: "10px",
-										},
+									"&::-webkit-scrollbar-thumb": {
+										backgroundColor: `red.600`,
+										borderRadius: "10px",
+									},
 								}}
 								overflow={"auto"}
 								minH="30vh"
@@ -77,53 +128,42 @@ const ToDoList: React.FC = () => {
 									flexDirection="column"
 									w="100%"
 									gap={1}>
-									{content.map(
-										(todo, indexJ) => (
-											<ListItem
-												w="100%"
-												key={indexJ}>
-												<Flex>
-													<Text
-														as="label"
-														htmlFor={`${todo.id}-${indexJ}`}
-														cursor={
-															"pointer"
-														}
-														w="100%">
-														<ToDoItem
-															todo={
-																todo
-															}
-														/>
-													</Text>
-												</Flex>
-												{indexJ <
-													content.length -
-														1 && (
-													<Divider />
-												)}
-											</ListItem>
-										)
-									)}
+									{content.map((todo, indexJ) => (
+										<ListItem w="100%" key={indexJ}>
+											<Flex>
+												<Text
+													as="label"
+													htmlFor={`${todo.id}-${indexJ}`}
+													cursor={"pointer"}
+													w="100%">
+													<ToDoItem
+														todo={todo}
+													/>
+												</Text>
+											</Flex>
+											{indexJ <
+												content.length - 1 && (
+												<Divider />
+											)}
+										</ListItem>
+									))}
 								</List>
 							</Container>
 						</TabPanel>
 					))}
 				</TabPanels>
 			</Tabs>
-
-			<Flex
-				align={"center"}
-				direction="column"
-				w="100%"
-				gap={4}>
+			<Text align="center" fontSize={"xl"}>
+				WIP Footer Toolbar
+			</Text>
+			{/* <Flex align={"center"} direction="column" w="100%" gap={4}>
 				<Text size={"xl"}>0 Selected</Text>
 				<Flex w="100%" justifyContent={"space-evenly"}>
 					<Button>Delete</Button>
 					<Button>Is Done</Button>
 					<Button>To Do</Button>
 				</Flex>
-			</Flex>
+			</Flex> */}
 		</Stack>
 	);
 };
